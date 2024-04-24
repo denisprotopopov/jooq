421f22b8a29b0eb8d91bd9c3590db3c54a7b33ec
Denis Protopopov <snu_ppy@outlook.com>
2024-04-24 21:30:53 +0400

    Update EdServiceImpl.kt

--- EdServiceImpl.kt
+++ EdServiceImpl.kt
@@ -3,10 +3,7 @@ package ru.raiffeisen.rise.core.service.impl
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import ru.raiffeisen.rise.core.controller.dto.EdDto
-import ru.raiffeisen.rise.core.repository.EdRepository
-import ru.raiffeisen.rise.core.repository.model.Ed
-import ru.raiffeisen.rise.core.service.EdService
+
 
 @Service
 class EdServiceImpl(
