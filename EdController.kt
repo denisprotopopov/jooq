e61d370f91db850c12a8c9e6e7a4442ab0aee517
Denis Protopopov <snu_ppy@outlook.com>
2024-04-24 21:30:37 +0400

    Update EdController.kt

--- EdController.kt
+++ EdController.kt
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import ru.raiffeisen.rise.core.controller.RestConstants.REST_API_PREFIX
-import ru.raiffeisen.rise.core.controller.dto.EdDto
-import ru.raiffeisen.rise.core.repository.model.Ed
-import ru.raiffeisen.rise.core.service.EdService
+
 
 @RestController
 @RequestMapping("$REST_API_PREFIX/ed")
